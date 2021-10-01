30a03415d8629bad392c5b9f5b04f4c596ca68f3
Shah <abhinav.shah@nokia.com>
2021-10-01 14:11:22 -0400

    Initial commit for classic snake game

---
+++ scoreboard.py
@@ -0,0 +1,40 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.score = 0
+        with open("data.txt") as file:
+            content = file.read()
+            self.high_score = int(content)
+        self.penup()
+        self.goto(0, 260)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def update_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
